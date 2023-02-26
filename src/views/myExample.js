3d661eb1fb12dcb330adfbfdf9ed03069716af4a
NGUYEN VAN CUONG <nvcengineer@gmail.com>
2023-02-26 10:08:13 +0700

    form

--- src/views/myExample.js
+++ src/views/myExample.js
@@ -1,20 +1,47 @@
 import React from 'react';
 
 class MyExample extends React.Component {
-    handleClickButton = () => {
-        alert(`You have just click me`)
+    state = {
+        firstName: '',
+        lastName: '',
+    }
+    handleChangFirstName = (event) => {
+        this.setState({
+            firstName: event.target.value
+        })
+    }
+    handleChangLastName = (event) => {
+        this.setState({
+            lastName: event.target.value
+        })
+    }
+    handleSubmit = (event) => {
+        event.preventDefault()  // Ngan chan refresh trang
+        alert('You have just click me')
     }
     render() {
 
         return (
             <>
-                <div className='first'>
-                    My name is Hoang Cuong
-                </div>
-                <div className='second'>
-                    <button onClick={() => this.handleClickButton()}>Click me</button>
+                <h2> Sign up now</h2>
+                <form>
+                    <label> First name</label> <br />
+                    <input
+                        type='text'
+                        value={this.state.firstName}
+                        onChange={(event) => this.handleChangFirstName(event)}
 
-                </div>
+                    /><br />
+                    <label> Last name</label><br />
+                    <input type='text'
+                        value={this.state.lastName}
+                        onChange={(event) => this.handleChangLastName(event)}
+                    ></input><br />
+                    <input type='button' value="Submit"
+                        onClick={(event) => this.handleSubmit(event)}
+                    ></input><br />
+
+                </form>
             </>
         )
     }
